d5bf8a4c4623f4211b99094d6d3caee730709c7c
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-29 15:19:36 -0500

    Task 4

---
+++ holbertonschool-low_level_programming/hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print a specifyc Text on screen using puts
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
