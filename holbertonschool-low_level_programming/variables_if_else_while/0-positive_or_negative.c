4c7356d0d2a20f21c075a6a75f3fa051e7ab20d1
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-30 10:54:39 -0500

    Task 0

---
+++ holbertonschool-low_level_programming/variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Lalo Rdz
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Assign a random number to the variable n each time it is executed.
+ *
+ * Description: Positive anything is better than negative nothing.
+ * Return: Zero.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	return (0);
+}
