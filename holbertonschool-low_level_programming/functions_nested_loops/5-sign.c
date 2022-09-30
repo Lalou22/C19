7f8e908d6a406fe69026b59120ee61889f457466
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-30 12:42:52 -0500

    Task 5

---
+++ holbertonschool-low_level_programming/functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Check if a int is post, negt or zero
+ * @n: Int to be checked
+ *
+ * Description: Write a function that prints the sign of a number.
+ * Return: -1, 1 or 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (0);
+}
