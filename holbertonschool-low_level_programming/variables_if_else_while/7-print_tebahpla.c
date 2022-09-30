459267ad47d0bf2b99c2721175b40d94d336b8a6
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-30 11:13:29 -0500

    Task 7

---
+++ holbertonschool-low_level_programming/variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Lalo Rdz
+ */
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
