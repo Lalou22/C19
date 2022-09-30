b7819cdb5b41a1ec8dc919d29cfb00cea2c3ce46
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-30 10:56:07 -0500

    Task 1

---
+++ holbertonschool-low_level_programming/variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Lalo Rdz
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Assign a random number to the variable n each time it is executed.
+ *
+ * Description: Print the last digit of the number stored in the variable n.
+ * Return: NULL.
+ */
+int main(void)
+{
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last < 0)
+		last = last * -1;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	if (last < 6 && last != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	return (0);
+}
