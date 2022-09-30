7b85a7457b4f3609ffc8a18d1c2286100fc81659
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-09-30 11:08:12 -0500

    Task 5

---
+++ holbertonschool-low_level_programming/variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Lalo Rdz
+ */
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
