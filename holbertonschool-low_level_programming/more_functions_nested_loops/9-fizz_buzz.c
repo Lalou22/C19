ccd1d75c34c2d953d9dc467112faf8cd39242e8b
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-03 13:10:58 -0500

    Task 9

--- holbertonschool-low_level_programming/more_functions_nested_loops/9-fizz_buzz.c
+++ holbertonschool-low_level_programming/more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,16 @@ int main(void)
 {
 	int i;
 
-	printf("0");
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		printf(" ");
 		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d", i);
 		if (i % 3 == 0)
 			printf("Fizz");
 		if (i % 5 == 0)
 			printf("Buzz");
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
