77c7343e1b8da83d87b31155f2a97c269a2be3a4
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-03 11:18:37 -0500

    Task 6

--- holbertonschool-low_level_programming/functions_nested_loops/main.h
+++ holbertonschool-low_level_programming/functions_nested_loops/main.h
@@ -4,11 +4,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/* Task 0 */
 int _putchar(char c);
+/* Task 1 */
 void print_alphabet(void);
+/* Task 2 */
 void print_alphabet_x10(void);
+/* Task 3 */
 int _islower(int c);
+/* Task 4 */
 int _isalpha(int c);
+/* Task 5 */
 int print_sign(int n);
+/* Task 6 */
+int _abs(int);
 
 #endif /* MAIN_H */
