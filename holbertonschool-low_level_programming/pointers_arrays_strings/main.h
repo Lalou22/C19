abd72bc7653c8c8f97cbe2ac0864bf607966b3ed
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-11 00:02:20 -0500

    Task 5 - Project 2

--- holbertonschool-low_level_programming/pointers_arrays_strings/main.h
+++ holbertonschool-low_level_programming/pointers_arrays_strings/main.h
@@ -41,5 +41,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 /* Task 4 */
 void reverse_array(int *a, int n);
+/* Task 5 */
+char *string_toupper(char *);
 
 #endif /* MAIN_H */
