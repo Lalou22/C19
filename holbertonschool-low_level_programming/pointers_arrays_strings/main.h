861126fc550a63f46b08d962eae63c09d20eb7c2
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-11 15:25:24 -0500

    Task 6 - Project 3

--- holbertonschool-low_level_programming/pointers_arrays_strings/main.h
+++ holbertonschool-low_level_programming/pointers_arrays_strings/main.h
@@ -67,5 +67,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 /* Task 5 */
 char *_strstr(char *haystack, char *needle);
+/* Task 6 */
+void print_chessboard(char (*a)[8]);
 
 #endif /* MAIN_H */
