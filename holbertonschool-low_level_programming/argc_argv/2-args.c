8a2f20ac6f571ee2d8b0e42cbd3afb48cbf5753c
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-12 09:22:30 -0500

    Task 2 - Using _putchar not standard library

---
+++ holbertonschool-low_level_programming/argc_argv/2-args.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-args.c
+ * Auth: Lalo Rdz
+ */
+#include "main.h"
+
+/**
+ * main - Write a program that prints all arguments it receives.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of strings. Each is one of the arguments that was passed.
+ *
+ * Description: Prints all arguments.
+ * Return: Zero.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	for (i = 0; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			_putchar(argv[i][j]);
+			j++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
