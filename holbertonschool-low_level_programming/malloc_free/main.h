1d32c5c67127b7327b5e37ba8b224404322a2400
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-12 10:06:36 -0500

    Task 0

---
+++ holbertonschool-low_level_programming/malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+/* Task 1 */
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
