a394f0a631a83be7e01ea5a680a31b79987de4ce
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-10-21 13:13:59 -0500

    Task 2

--- holbertonschool-low_level_programming/more_malloc_free/main.h
+++ holbertonschool-low_level_programming/more_malloc_free/main.h
@@ -4,11 +4,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 /* Task 0 */
 void *malloc_checked(unsigned int b);
 /* Task 1 */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+/* Task 2 */
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* MAIN_H */
